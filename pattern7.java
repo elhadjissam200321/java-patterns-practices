e1b33290596eea520d3053c46f24be33390ac4b2
ELHADJ ISSAM <elhadjissam200321@gmail.com>
2025-03-09 21:21:22 +0000

    pattern7.java
     1 2 3 4 5
     6 7 8 9 10
     11 12 13 14 15
     16 17 18 19 20
     21 22 23 24 25

---
+++ pattern7.java
@@ -0,0 +1,24 @@
+/*
+Expected Output:
+ 1 2 3 4 5
+ 6 7 8 9 10
+ 11 12 13 14 15
+ 16 17 18 19 20
+ 21 22 23 24 25
+
+***** ***** ***** ***** *****
+*/ 
+
+class Main {
+    public static void main(String[] args) {
+        int k=1;
+        for(int i=1;i<=5;i++){
+            
+            for(int j=1;j<=5;j++){
+                System.out.print(" "+k);
+                k=k+2;
+            }
+            System.out.println();
+        }
+    }
+}
