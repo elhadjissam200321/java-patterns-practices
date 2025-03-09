0c87a63de38be2e5f188d8b2298e97074873a2dc
ELHADJ ISSAM <elhadjissam200321@gmail.com>
2025-03-09 21:24:35 +0000

    pattern9.java
     1 2 3 4 5
     2 4 6 8 10
     3 6 9 12 15
     4 8 12 16 20
     5 10 15 20 25

---
+++ pattern9.java
@@ -0,0 +1,24 @@
+/*
+Expected Output:
+ 1 2 3 4 5
+ 2 4 6 8 10
+ 3 6 9 12 15
+ 4 8 12 16 20
+ 5 10 15 20 25
+
+
+***** ***** ***** ***** *****
+*/ 
+
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            
+            for(int j=1;j<=5;j++){
+                System.out.print(" "+i*j);
+            }
+            System.out.println();
+        }
+    }
+}
